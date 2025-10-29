66832eb50f461a36f55d4542e9cb7413b59ea901
Syed Mashiur Rahman <syedshaon99@gmail.com>
2025-10-29 09:56:42 +0600

    config update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { resolve } from "path";
 import handlebars from "vite-plugin-handlebars";
 
 export default defineConfig({
+  base: "/clark-mayer/",
   plugins: [
     handlebars({
       partialDirectory: resolve(__dirname, "src/partials"),
